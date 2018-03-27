d91826ba34f84b56b4646cc8baf8f90237025f2d
1543597852@qq.com <1543597852@qq.com>
2018-03-27 13:44:20 +0800

    21321

--- js/index.js
+++ js/index.js
@@ -10,89 +10,81 @@ if(window.innerWidth>600){
 }
 var videos = new Array(50);
 //播放完成
-function endedHandler() {
-	alert('可以投票了')
+function endedHandler(me) {
+	var inputs = me.parentNode.querySelectorAll('input');
+	console.log(me.parentNode.querySelectorAll('input'))
+	for(var i = 0;i<inputs.length;i++){
+		inputs[i].removeAttribute('disabled')
+	}
 }
 function addVideo(num, container, poster, video) {
 	videos[num] = document.querySelector(container);
 	videos[num].poster = poster;
 	videos[num].src=video;
-	if ('ontouchstart' in window) {
-		var me = videos[num];
-		videos[num].addEventListener('touchstart',function(){
-			if(me.paused){
-				me.play();
-				me.last_time = 0;
-				videos[num].timer = setInterval(function(){
-					if(me.currentTime-me.last_time>1){
-						me.currentTime = me.last_time;
-					}
-					me.last_time = me.currentTime;
-				},500)
-			}else{
-				me.pause();
-			}
-		});
-	}else{
-		console.log('22222222222')
+	if(!(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent))){
 		videos[num].addEventListener('click',function(){
-			var me = this;
-			if(me.paused){
-				me.play();
-				me.last_time = 0;
-				videos[num].timer = setInterval(function(){
-					if(me.currentTime-me.last_time>1){
-						me.currentTime = me.last_time;
-					}
-					me.last_time = me.currentTime;
-				},500)
+			if(this.paused){
+				this.play();
 			}else{
-				me.pause();
+				this.pause();
 			}
-		});
+		})
 	}
+	videos[num].addEventListener('play',function(){
+		var me = this;
+		videos[num].timer = setInterval(function(){
+			if(me.currentTime-me.last_time>1){
+				me.currentTime = me.last_time;
+			}
+			me.last_time = me.currentTime;
+		},500)
+	});
+	videos[num].addEventListener('pause',function(){
+		var me = this;
+		videos[num].timer = setInterval(function(){
+			if(me.currentTime-me.last_time>1){
+				me.currentTime = me.last_time;
+			}
+			me.last_time = me.currentTime;
+		},500);
+		clearInterval(this.timer);
+	});
 	videos[num].addEventListener('ended',function(){
-		endedHandler();
+		if(this.currentTime-this.last_time<1){
+			endedHandler(this);
+			clearInterval(this.timer);
+		}
 	});
 }
-addVideo(1, '.video1', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(2, '.video2', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(3, '.video3', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(4, '.video4', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(5, '.video5', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(6, '.video6', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(7, '.video7', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(8, '.video8', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(9, '.video9', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(10, '.video10', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(11, '.video11', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(12, '.video12', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(13, '.video13', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(14, '.video14', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(15, '.video15', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(16, '.video16', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(17, '.video17', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(18, '.video18', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(19, '.video19', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(20, '.video20', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(21, '.video21', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(22, '.video22', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(23, '.video23', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(24, '.video24', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(25, '.video25', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(26, '.video26', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(27, '.video27', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
-addVideo(28, '.video28', 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
+var template = '';
+var name = '电影名'
+for(var i = 1;i<=32;i++){
+	template+='<div class="video">'+
+			'<p>'+i+'.'+name+'</p>'+
+			'<video class="video'+i+'" controls="controls"></video>'+
+			'<span>最佳作品<input type="checkbox" name="works" disabled="disabled" onclick="checkBox(this)"></span>'+
+			'<span>最佳导演<input type="checkbox" name="director" disabled="disabled" onclick="checkBox(this)"></span>'+
+			'<span>最佳编剧<input type="checkbox" name="screenwriter" disabled="disabled" onclick="checkBox(this)"></span>'+
+		'</div>';
+}
+document.querySelector('.content').innerHTML+=template;
+console.log(document.querySelector('.content'))
+for(var i = 1;i<=32;i++){
+	addVideo(i, '.video'+i, 'img/timg.jpg', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4');
+}
 
 function checkBox(e) {
 	switch (e.getAttribute('name')) {
-		case 'work':
+		case 'works':
+			console.log('这是第'+e.parentNode.parentNode.querySelector('video').className.substring(5)+'个视频');
 			console.log('最佳作品');
 			break;
 		case 'director':
+			console.log('这是第'+e.parentNode.parentNode.querySelector('video').className.substring(5)+'个视频');
 			console.log('最佳导演');
 			break;
 		case 'screenwriter':
+			console.log('这是第'+e.parentNode.parentNode.querySelector('video').className.substring(5)+'个视频');
 			console.log('最佳导演');
 			break;
 		default:
